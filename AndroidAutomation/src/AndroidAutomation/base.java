914692f482f65a7515c7bc5f4ef9502bc913b4f5
nhdrasel <nhdrasel@gmail.com>
2022-04-08 10:51:45 +1200

    Rebase

--- AndroidAutomation/src/AndroidAutomation/base.java
+++ AndroidAutomation/src/AndroidAutomation/base.java
@@ -4,9 +4,8 @@ public class base {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("");
-		System.out.println("");
-		README.md;
+	
+		sadgklj;
 	}
 
 }
