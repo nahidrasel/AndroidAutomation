60f653c0b039890a61aafc872153619174b42e58
nhdrasel <nhdrasel@gmail.com>
2022-04-14 11:38:23 +1200

    TestNg Convert

---
+++ MobileAutomation/src/AppiumFramework/base.java
@@ -0,0 +1,48 @@
+
+package AppiumFramework;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.time.Duration;
+import java.util.Properties;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+public class base {
+
+	public static AndroidDriver capabilities(String appName) throws IOException {
+
+		System.getProperty("user.dir");
+		FileInputStream fis = new FileInputStream(
+				System.getProperty("user.dir") + "\\src\\AppiumFramework\\global.properties");
+		Properties prop = new Properties();
+		prop.load(fis);
+
+		File appDir = new File("src");
+
+		File app = new File(appDir, (String) prop.get(appName));
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+		String device = (String) prop.get("device");
+
+		// capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"Nahidemulator");
+
+		capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, device);
+
+		capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2");// new step
+
+		capabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 14);
+
+		capabilities.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
+
+		AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		return driver;
+	}
+
+}
---
+++ MobileAutomation/src/AppiumFramework/ecommerce_tc_4.java
@@ -0,0 +1,100 @@
+package AppiumFramework;
+
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.touch.TouchActions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+
+public class ecommerce_tc_4 extends base {
+	@Test
+	public void totalValidation() throws InterruptedException, IOException {
+
+		AndroidDriver driver = capabilities("GeneralStoreApp");
+
+		driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).sendKeys("Hello");
+
+		driver.hideKeyboard();
+
+		driver.findElement(By.xpath("//*[@text='Female']")).click();
+
+		driver.findElement(By.id("android:id/text1")).click();
+
+		driver.findElement(AppiumBy.androidUIAutomator(
+				"new UiScrollable(new UiSelector().scrollable(true)).scrollIntoView(text(\"Argentina\"))"));
+
+		// driver.findElement(MobileBy.AndroidUIAutomator("new UiScrollable(new
+		// UiSelector().scrollable(true).instance(0)).scrollIntoView(new
+		// UiSelector().textMatches(\"" + containedText + "\").instance(0))"));
+
+		driver.findElement(By.xpath("//*[@text='Argentina']")).click();
+
+		driver.findElement(By.id("com.androidsample.generalstore:id/btnLetsShop")).click();
+
+		driver.findElements(By.xpath("//*[@text='ADD TO CART']")).get(0).click();
+		driver.findElement(By.xpath("//*[@text='ADD TO CART']")).click();
+		driver.findElement(By.id("com.androidsample.generalstore:id/appbar_btn_cart")).click();
+		Thread.sleep(4000);
+
+		int count = driver.findElements(By.id("com.androidsample.generalstore:id/productPrice")).size();
+
+		double sum = 0;
+
+		for (int i = 0; i < count; i++)
+
+		{
+
+			String amount1 = driver.findElements(By.id("com.androidsample.generalstore:id/productPrice")).get(i)
+					.getText();
+
+			double amount = getAmount(amount1);
+
+			sum = sum + amount;// 280.97+116.97
+
+		}
+
+		System.out.println(sum + " sum of products");
+
+		String total = driver.findElement(By.id("com.androidsample.generalstore:id/totalAmountLbl")).getText();
+
+		total = total.substring(1);
+
+		double totalValue = Double.parseDouble(total);
+
+		System.out.println(totalValue + " Total value of products");
+
+		Assert.assertEquals(sum, totalValue);
+
+		// Mobile Gestures
+
+		driver.findElement(By.className("android.widget.CheckBox")).click();
+
+		WebElement tc = driver.findElement(By.xpath("//*[@text='Please read our terms of conditions']"));
+
+		TouchActions t = new TouchActions(driver);
+		t.clickAndHold(tc).perform();
+		driver.findElement(By.id("android:id/button1")).click();
+
+		driver.findElement(By.id("com.androidsample.generalstore:id/btnProceed")).click();
+	}
+	
+	
+
+	public static double getAmount(String value)
+
+	{
+
+		value = value.substring(1);
+
+		double amount2value = Double.parseDouble(value);
+
+		return amount2value;
+
+	}
+
+}
